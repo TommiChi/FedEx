b673e0380aaeb542645be20d03b9a7e88ac24383
Tom Nchinda <tomnchinda@gmail.com>
2022-11-07 10:29:41 +0100

    create new docs branch

--- src/test-cases.js
+++ src/test-cases.js
@@ -4,7 +4,7 @@ const urls = {
     production: [
         'https://www.fedex.com/lite/lite-ship.html?locale=en_nl&cntry_code=nl_english#address',
         'https://www.fedex.com/en-nl/online/rating.html#',
-        // 'https://www.fedex.com/en-nl/shipping/schedule-pickup.html#form',
+        'https://www.fedex.com/en-nl/shipping/schedule-pickup.html#form',
         // 'https://www.fedex.com/en-nl/shipping/industry-solutions/ecommerce.html',
         // 'https://www.fedex.com/',
     ]
