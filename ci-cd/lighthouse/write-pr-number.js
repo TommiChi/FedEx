c0113adc6058aa3ee694e456f6a4d557cd586b25
CI Automation <ci.automation@fedex.com>
2022-12-14 10:38:38 +0100

    pr

--- ci-cd/lighthouse/write-pr-number.js
+++ ci-cd/lighthouse/write-pr-number.js
@@ -2,9 +2,13 @@ const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const { PR_PATH } = process.env;
+const { PR_NUMBER, REF } = process.env;
 
 console.warn('!!!!!!!!!!!!!!!\n', PR_PATH);
 console.warn('!!!!!!!!!!!!!!!\n', PR_PATH.split('/').reverse().find((part) => /^(\d+)$/.test(part)));
 
-fs.writeFileSync(path.join(__dirname, '..', '..', 'pr-number.txt'), PR_PATH.split('/').reverse().find((part) => /^(\d+)$/.test(part)), { encoding: 'utf8' });
+const isCreatedPR = REF.indexOf('/pull/') > -1;
+
+if (!isCreatedPR) return;
+
+fs.writeFileSync(path.join(__dirname, '..', '..', 'pr-number.txt'), PR_NUMBER, { encoding: 'utf8' });
